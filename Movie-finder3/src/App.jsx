7f79005eec6fb25d7941783557fb78453c858a13
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-04-11 06:55:00 -0600

    setup movie finder for altcademy and netlify depoloy
    also set lapotop up for coding!

---
+++ Movie-finder3/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {Route, Routes} from 'react-router-dom'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import Movie from './components/Movie'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="404" element={<NotFound />} />
+      <Route path="/movie/:id" element={<Movie />} />
+
+    </Routes>
+  )
+}
+
+export default App
