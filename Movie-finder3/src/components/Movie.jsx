9def748387390ae17bedc6e8bd5e7a9a27c35156
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-04-12 07:01:43 -0600

    got it working

--- Movie-finder3/src/components/Movie.jsx
+++ Movie-finder3/src/components/Movie.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Movie = () => {
   const { id } = useParams();
+  const [movieData, setMovieData] = useState({});
 
-  React.useEffect(() => {
+  useEffect(() => {
     fetch(`https://www.omdbapi.com/?i=${id}&apikey=73740781`)
       .then((response) => response.json())
       .then((data) => {
-        console.table(data);
+        setMovieData(data);
+        console.table(movieData);
       });
   }, [id]);
 
   return (
     <div className="container">
       <div className="row">
-        <div className="col-12">Name</div>
+        <div className="col-12">Name {movieData.Title} </div>
+        <div className="col-12">Year {movieData.Year} </div>
+        <div className="col-12">Genre {movieData.Genre} </div>
+        <div className="col-12">Director {movieData.Director} </div>
+        <div className="col-12">Actors {movieData.Actors} </div>
+        <div className="col-12">Plot {movieData.Plot} </div>
+        <div className="col-12">
+          <img src={movieData.Poster} />{" "}
+        </div>
+        <div className="col-12">imdbRating {movieData.imdbRating} </div>
+        <div className="col-12">imdbVotes {movieData.imdbVotes} </div>
+        <div className="col-12">imdbID {movieData.imdbID} </div>
+        <div className="col-12">Type {movieData.Type} </div>
+        <div className="col-12">Response {movieData.Response} </div>
       </div>
     </div>
   );
